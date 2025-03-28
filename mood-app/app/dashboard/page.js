b08c0fbb149f9104bca19afe256817c78009307a
aleksa-vidojevic-pixel <aleksa.vidojevic.pixel@outlook.com>
2025-03-28 02:46:22 +0100

    Almost done

--- mood-app/app/dashboard/page.js
+++ mood-app/app/dashboard/page.js
@@ -1,28 +1,15 @@
 import Dashboard from "@/components/Dashboard";
-import Login from "@/components/Login";
 import Main from "@/components/Main";
 
 export const metadata = {
-    title: "Broodl ⋅ Dashboard",
-    description: "Track your daily mood, every day of the year!",
+    title: "Broodl · Dashboard",
 };
 
 export default function DashboardPage() {
-    const isAuthenticated = true;
-
-    let children = (
-        <Login />
-    );
-
-    if (isAuthenticated) {
-        children = (
-            <Dashboard />
-        )
-    }
 
     return (
         <Main>
-            {children}
+            <Dashboard />
         </Main>
     )
 }
